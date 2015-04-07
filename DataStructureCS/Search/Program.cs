4f05b64617526e88d99548744d16441964d5d516
BansalVikrant <BansalVikrant@gmail.com>
2015-04-06 20:13:46 -0700

    Lets do it

---
+++ DataStructureCS/Search/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    class Program
+    {
+        const int MAXLEN = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of elements");
+            int intLength = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter sorted array");
+            int[] answer = new int[MAXLEN];
+            for (int i = 0; i < intLength; i++)
+            {
+                answer[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
